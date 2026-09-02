ee6a5a3700aa6ed2da3ac5c7cf99624325b22148
Vasili Pascal <vasili.pascal@gmail.com>
2026-09-02 15:02:14 +0300

    chore: update gitlab, supabase configs
    - Updated stacker.yml configs
    - Fixed supabase roles.sql

--- stacker-projects/supabase/volumes/db/roles.sql
+++ stacker-projects/supabase/volumes/db/roles.sql
@@ -1,8 +1,14 @@
--- NOTE: change to your own passwords for production environments
-\set pgpass `echo "$POSTGRES_PASSWORD"`
-
-ALTER USER authenticator WITH PASSWORD :'pgpass';
-ALTER USER pgbouncer WITH PASSWORD :'pgpass';
-ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
-ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
-ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
+-- Set role passwords from POSTGRES_PASSWORD environment variable
+DO $$
+DECLARE
+  pw TEXT;
+BEGIN
+  pw := current_setting('POSTGRES_PASSWORD', true);
+  IF pw IS NOT NULL AND pw != '' THEN
+    EXECUTE format('ALTER USER authenticator WITH PASSWORD %L', pw);
+    EXECUTE format('ALTER USER pgbouncer WITH PASSWORD %L', pw);
+    EXECUTE format('ALTER USER supabase_auth_admin WITH PASSWORD %L', pw);
+    EXECUTE format('ALTER USER supabase_functions_admin WITH PASSWORD %L', pw);
+    EXECUTE format('ALTER USER supabase_storage_admin WITH PASSWORD %L', pw);
+  END IF;
+END $$;
